52fdb0acc897987587a1ddca303ba0aa75bd5f52
gabichoi <gabichoi96@gmail.com>
2018-03-26 08:11:57 -0700

    Update merge_sort.py

--- Array/merge_sort.py
+++ Array/merge_sort.py
@@ -1,22 +1,22 @@
 # Merge sort algorithm
 
-def mergesort(seq):
+def merge_sort(arr):
 
     """
-    >>> mergesort([4, 2, 5, 2, 1])
+    >>> merge_sort([4, 2, 5, 2, 1])
     [1, 2, 2, 4, 5]
-    >>> mergesort([])     # sorting an empty list
+    >>> merge_sort([])     # sorting an empty list
     []
-    >>> mergesort([1])   # sorting a one-element list
+    >>> merge_sort([1])   # sorting a one-element list
     [1]
 
     """ 
-    if seq == []:
+    if arr == []:
         return []
-    elif len(seq) == 1:
-        return seq 
+    elif len(arr) == 1:
+        return arr 
     else:
-        mid = (len(seq) // 2)
-        first_half = seq[0 : mid]
-        second_half = seq[mid : len(seq)]
+        mid = (len(arr) // 2)
+        first_half = arr[0 : mid]
+        second_half = arr[mid : len(arr)]
         return merge (mergesort(first_half), mergesort(second_half))
