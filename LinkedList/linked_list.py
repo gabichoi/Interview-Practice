0c5f3d74e561f076957871b736ab062302aaf329
gabichoi <gabichoi96@gmail.com>
2018-03-26 12:28:46 -0700

    Create linked_list.py

---
+++ LinkedList/linked_list.py
@@ -0,0 +1,51 @@
+# Linked List Class
+class Link:
+    """
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s
+    Link(1, Link(2, Link(3)))
+    >>> len(s)
+    3
+    >>> s[2]
+    3
+    >>> s = Link.empty
+    >>> len(s)
+    0
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_str = ', ' + repr(self.rest)
+        else:
+            rest_str = ''
+        return 'Link({0}{1})'.format(repr(self.first), rest_str)
+
+    def __len__(self):
+        """ Return the number of items in the linked list.
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> len(s)
+        3
+        >>> s = Link.empty
+        >>> len(s)
+        0
+        """
+        return 1 + len(self.rest)
+
+    def __getitem__(self, i):
+        """Returning the element found at index i.
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> s[1]
+        2
+        >>> s[2]
+        3
+        """
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i-1]
