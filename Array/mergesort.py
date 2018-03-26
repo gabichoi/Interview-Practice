1d669e1115d46c8748e3db090cfda08d07d17f3f
gabichoi <gabichoi96@gmail.com>
2018-03-26 07:30:47 -0700

    Add files via upload

---
+++ Array/mergesort.py
@@ -0,0 +1,22 @@
+# Given a list, returns it sorted. Mergesort algorithm
+
+def mergesort(seq):
+
+    """
+    >>> mergesort([4, 2, 5, 2, 1])
+    [1, 2, 2, 4, 5]
+    >>> mergesort([])     # sorting an empty list
+    []
+    >>> mergesort([1])   # sorting a one-element list
+    [1]
+
+    """ 
+    if seq == []:
+        return []
+    elif len(seq) == 1:
+        return seq 
+    else:
+        mid = (len(seq) // 2)
+        first_half = seq[0 : mid]
+        second_half = seq[mid : len(seq)]
+        return merge (mergesort(first_half), mergesort(second_half))
